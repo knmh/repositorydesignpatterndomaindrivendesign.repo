9a4704cf36585dcbd24201c06712059e9109275b
hediyeh <hediyehknm@gmail.com>
2024-01-27 18:42:45 +0330

    rc

--- Asp.NetRepositoryDesignPattern/RepositoryDesignPatternDomainDrivenDesign/obj/Debug/net8.0/RepositoryDesignPatternDomainDrivenDesign.AssemblyInfo.cs
+++ Asp.NetRepositoryDesignPattern/RepositoryDesignPatternDomainDrivenDesign/obj/Debug/net8.0/RepositoryDesignPatternDomainDrivenDesign.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepositoryDesignPatternDomainDrivenDesign")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f6b27364aa5d78f95e4dfd35bd33528a1f3a471")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepositoryDesignPatternDomainDrivenDesign")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepositoryDesignPatternDomainDrivenDesign")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
